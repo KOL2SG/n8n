468c73d3f4c4368d2b9de53c68a3e19173cbb9ba
Philipp <Philipp.Goetzinger@gmail.com>
2025-05-12 17:43:58 +0800

    agent global

---
+++ packages/cli/src/bootstrap-proxy.ts
@@ -0,0 +1,11 @@
+import 'global-agent/bootstrap';
+import axios from 'axios';
+
+axios.interceptors.request.use((config) => {
+	// if someone's set a core HTTPS.Agent that isn't for skip-SSL, drop it
+	const agent = (config as any).httpsAgent;
+	if (agent instanceof require('https').Agent && agent.options.rejectUnauthorized !== false) {
+		delete (config as any).httpsAgent;
+	}
+	return config;
+});
