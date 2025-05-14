3354f1dd62cb2c32d8c978666e049c3c91c6602e
Philipp <Philipp.Goetzinger@gmail.com>
2025-05-14 08:00:24 +0800

    after cleanup

--- packages/cli/src/bootstrap-proxy.ts
+++ packages/cli/src/bootstrap-proxy.ts
@@ -1,6 +1,5 @@
 console.log('[bootstrap-proxy] interceptor loaded');
 import 'global-agent/bootstrap';
-import axios from 'axios';
 
 console.error(
 	'[bootstrap-proxy] env HTTP_PROXY=',
@@ -10,22 +9,3 @@ console.error(
 	'GLOBAL_AGENT.HTTP_PROXY=',
 	(global as any).GLOBAL_AGENT?.HTTP_PROXY,
 );
-
-axios.interceptors.request.use((config) => {
-	console.error(
-		'[bootstrap-proxy] about to send:',
-		config.method?.toUpperCase(),
-		config.url,
-		'via proxy?',
-		!!process.env.HTTPS_PROXY,
-		'customAgent?',
-		!!(config as any).httpsAgent,
-	);
-	// if someone's set a core HTTPS.Agent that isn't for skip-SSL, drop it
-	const agent = (config as any).httpsAgent;
-	if (agent instanceof require('https').Agent && agent.options.rejectUnauthorized !== false) {
-		delete (config as any).httpsAgent;
-	}
-	delete (config as any).httpAgent;
-	return config;
-});
